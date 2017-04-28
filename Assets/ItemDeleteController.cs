675c178348d4553dff7a35b6a84c3b6a80c5fa53
今堀耕一 <mokka_dogonsui@yahoo.co.jp>
2017-04-28 16:30:28 +0900

    プロジェクトStepAsideUnityChan　課題作成
    Lesson6の課題：不要アイテムの削除　
    unityちゃんに追随して動く壁を追加、壁がアイテムを削除

---
+++ Assets/ItemDeleteController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDeleteController : MonoBehaviour
+{
+    //Unityちゃんのオブジェクト
+    private GameObject unitychan;
+    //Unityちゃんと不要アイテム消去オブジェクトの距離
+    private float difference;
+
+    // Use this for initialization
+    void Start()
+    {
+        //Unityちゃんのオブジェクトを取得
+        this.unitychan = GameObject.Find("unitychan");
+        //Unityちゃんと不要アイテム消去オブジェクト（z座標）の差を求める
+        this.difference = unitychan.transform.position.z - this.transform.position.z;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Unityちゃんの位置に合わせて不要アイテム消去オブジェクトの位置を移動
+        this.transform.position = new Vector3(0, this.transform.position.y, this.unitychan.transform.position.z - difference);
+    }
+
+    //トリガーモードで他のオブジェクトと接触した場合の処理
+    void OnTriggerEnter(Collider other)
+    {
+        //コイン、車、コーンに衝突した場合
+        if (other.gameObject.tag == "CoinTag" || other.gameObject.tag == "CarTag" || other.gameObject.tag == "TrafficConeTag")
+        {
+            //接触したコインのオブジェクトを破棄
+            Destroy(other.gameObject);
+        }
+    }
+}
